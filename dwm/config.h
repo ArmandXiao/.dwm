8fa672ddaa3802d199216c683f138d6d4ca99cda
ArmandXiao <xiao_lingao@outlook.com>
2021-02-20 22:29:25 +0800

    patch 'vanitygap' to control gaps between windows. Now a single window has gaps also! gappx is set to 0, and should be no longer used, as it will conflicts with vanitygap patch.

--- dwm/config.h
+++ dwm/config.h
@@ -10,12 +10,15 @@
 static const unsigned int borderpx = 2;   /* border pixel of windows */
 static const unsigned int snap     = 32;  /* snap pixel */
 
-static const unsigned int gappih   = 20;
-static const unsigned int gappiv   = 10;
-static const unsigned int gappoh   = 20;
-static const unsigned int gappov   = 10;
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 
-static const unsigned int gappx    = 6;   /* pixel gap between clients */
+// unused
+static const unsigned int gappx    = 0;   // [> pixel gap between clients <]
+
 static const int showbar           = 1;   /* 0 means no bar */
 static const int topbar            = 1;   /* 0 means bottom bar */
 static const int horizpadbar       = 6;   /* horizontal padding for statusbar */
