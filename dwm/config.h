93997fae31728237018e9bca6ab7f871b1eaa9b9
ArmandXiao <xiao_lingao@outlook.com>
2022-02-16 00:32:41 +0800

    change the outer gap from 20 to 10

--- dwm/config.h
+++ dwm/config.h
@@ -12,7 +12,7 @@ static const unsigned int snap     = 32;  /* snap pixel */
 
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 
